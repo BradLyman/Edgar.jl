a8f4dc0a4c5207f87f21d37c158eeae9c3ecfc1a
Documenter.jl <documenter@juliadocs.github.io>
2020-11-24 19:32:11 +0000

    build based on 2cdeb89

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Edgar","category":"page"},{"location":"#Edgar","page":"Home","title":"Edgar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A discord bot for organizing Trial runs in ESO.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Edgar]","category":"page"}]
+}
